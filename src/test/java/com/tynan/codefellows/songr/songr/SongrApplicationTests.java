2fc2bbd5b9bf5f8c1e7455710391782caf2635da
Peter Tynan <pettynan@amazon.com>
2019-06-12 10:55:15 -0700

    initial commit

---
+++ src/test/java/com/tynan/codefellows/songr/songr/SongrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tynan.codefellows.songr.songr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SongrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
